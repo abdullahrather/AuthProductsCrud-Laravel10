4f575919f621375a64cf781c3cd2660941e6a85d
Abdullah Rather <abdullahrather19@gmail.com>
2023-07-25 12:48:42 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,19 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/register', [AuthController::class, 'registerPost'])->name('register.store');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login.post');
+    Route::get('/', [AuthController::class, 'register'])->name('register');
+    Route::post('/', [AuthController::class, 'registerPost'])->name('register.store');
+});
+
 
 Route::group(['middleware' => 'auth'], function () {
 
